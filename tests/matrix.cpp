050c6a48096f2b445c305b44c65331c649ae37a1
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-03-04 03:05:30 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -29,3 +29,203 @@ TEST_CASE("reading matrix")
     
     REQUIRE( input == ostream.str() );
 }
+
+TEST_CASE("add matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string stroka2{
+        "3, 4\n"
+        "7 2 2 7\n"
+        "1 5 2 6\n"
+        "2 2 8 5" };
+    std::string strokar{
+        "3, 4\n"
+        "8 3 6 12\n"
+        "3 6 4 12\n"
+        "11 5 11 12" };
+    
+    matrix_t matrix1, matrix2, result;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    result = matrix1 + matrix2;
+    
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
+
+TEST_CASE("sub matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "8 3 6 12\n"
+        "3 6 4 12\n"
+        "11 5 11 12" };
+    std::string stroka2{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string strokar{
+        "3, 4\n"
+        "7 2 2 7\n"
+        "1 5 2 6\n"
+        "2 2 8 5" };
+    
+    matrix_t matrix1, matrix2, result;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    result = matrix1 - matrix2;
+    
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
+
+TEST_CASE("mul matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string stroka2{
+        "4, 3\n"
+        "7 2 2 \n"
+        "1 5 2 \n"
+        "2 2 8 \n" 
+        "1 1 1" };
+    std::string strokar{
+        "3, 3\n"
+        "21 20 41\n"
+        "25 19 28\n"
+        "79 46 55" };
+    
+    matrix_t matrix1, matrix2, result;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    result = matrix1 * matrix2;
+    
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
+
+TEST_CASE("selfsub matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "8 3 6 12\n"
+        "3 6 4 12\n"
+        "11 5 11 12" };
+    std::string stroka2{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string strokar{
+        "3, 4\n"
+        "7 2 2 7\n"
+        "1 5 2 6\n"
+        "2 2 8 5" };
+    
+    matrix_t matrix1, matrix2;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    matrix1 -= matrix2;
+    
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
+
+TEST_CASE("selfadd matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string stroka2{
+        "3, 4\n"
+        "7 2 2 7\n"
+        "1 5 2 6\n"
+        "2 2 8 5" };
+    std::string strokar{
+        "3, 4\n"
+        "8 3 6 12\n"
+        "3 6 4 12\n"
+        "11 5 11 12" };
+    
+    matrix_t matrix1, matrix2;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    matrix1 += matrix2;
+    
+    std::ostringstream ostream;
+    matrix1.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
+
+TEST_CASE("selfmul matrix")
+{
+    std::string stroka1{
+        "3, 4\n"
+        "1 1 4 5\n"
+        "2 1 2 6\n"
+        "9 3 3 7" }; 
+    std::string stroka2{
+        "4, 3\n"
+        "7 2 2 \n"
+        "1 5 2 \n"
+        "2 2 8 \n" 
+        "1 1 1" };
+    std::string strokar{
+        "3, 3\n"
+        "21 20 41\n"
+        "25 19 28\n"
+        "79 46 55" };
+    
+    matrix_t matrix1, matrix2;   
+    std::istringstream istream1{ stroka1 };
+    std::istringstream istream2{ stroka2 };
+    
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    
+    matrix1 *= matrix2;
+    
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( strokar == ostream.str() );
+}
